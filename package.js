9cfb42efcf77423910f6b2b361f7ff530fa100c9
Eugene <eugene@ensostudios.com.au>
2017-10-19 08:50:26 +1000

    Update fork details

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-	name: 'rendro:easy-pie-chart',
-	version: '2.1.7',
+	name: 'enso:enso-easy-pie-chart',
+	version: '2.2',
 	summary: 'Meteor package for jQuery easyPieChart plugin!',
-	git: 'https://github.com/dotansimha/easy-pie-chart.git',
+	git: 'https://github.com/EnsoStudios/easy-pie-chart.git',
 	documentation: null
 });
 
